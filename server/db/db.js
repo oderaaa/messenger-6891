767588ef5c7857975d2596c6d74a104bbb953f76
Odera Okoye <oderaaa@gmail.com>
2021-11-28 12:42:47 -0500

    ..

--- server/db/db.js
+++ server/db/db.js
@@ -3,4 +3,5 @@ const Sequelize = require("sequelize");
 const db = new Sequelize(process.env.DATABASE_URL || `postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@localhost:5432/messenger`, {
   logging: false
 });
+
 module.exports = db;
